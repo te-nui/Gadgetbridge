2aa5759356731440f620db7122d1e3a7c0112981
te-nui <f15ryoryota@gmail.com>
2026-07-05 13:13:50 +0900

    Update XiaomiWatchS5Coordinator.java

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/xiaomi/watchs5/XiaomiWatchS5Coordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/xiaomi/watchs5/XiaomiWatchS5Coordinator.java
@@ -1,1 +1,38 @@
+package nodomain.freeyourgadget.gadgetbridge.devices.xiaomi.watchs5;
 
+import java.util.regex.Pattern;
+
+import nodomain.freeyourgadget.gadgetbridge.R;
+import nodomain.freeyourgadget.gadgetbridge.devices.xiaomi.XiaomiCoordinator;
+
+public class XiaomiWatchS5Coordinator extends XiaomiCoordinator {
+    @Override
+    public int getDeviceNameResource() {
+        return R.string.devicetype_xiaomi_watch_s5;
+    }
+
+    @Override
+    public int getDefaultIconResource() {
+        return R.drawable.ic_device_miwatch;
+    }
+
+    @Override
+    public int getDisabledIconResource() {
+        return R.drawable.ic_device_miwatch_disabled;
+    }
+
+    @Override
+    public ConnectionType getConnectionType() {
+        return ConnectionType.BT_CLASSIC;
+    }
+
+    @Override
+    protected Pattern getSupportedDeviceName() {
+        return Pattern.compile("^Xiaomi Watch S5( eSIM)?( \\d{2}mm)? [0-9A-F]{4}$");
+    }
+
+    @Override
+    public boolean isExperimental() {
+        return true;
+    }
+}
